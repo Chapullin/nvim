a9cb557d4b7d09eefdb695f927abfb327c89d18d
Cristian Molinero <cristian.molinero@mercadolibre.com>
2024-10-04 19:05:36 -0300

    Add higher contrast to line numbers

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -6,5 +6,11 @@ return {
   -- Set the colorscheme to Catppuccin
   config = function()
     vim.cmd.colorscheme "catppuccin"
+    -- Set line number colors,
+    vim.api.nvim_set_hl(0, 'LineNr', { fg = '#DCDCDC' })  -- Light color for normal line numbers
+    vim.api.nvim_set_hl(0, 'CursorLineNr', { fg = '#FFD700' })  -- Bright color for current line number
+
+    -- Optional: Enable cursorline for better visibility
+    vim.opt.cursorline = true
   end
 }
