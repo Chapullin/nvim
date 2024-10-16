d2a1af1a028ab307385b7bd8103029047eaf3aa7
Cristian Molinero <cristian.molinero@mercadolibre.com>
2024-10-15 21:50:59 -0300

    add copilot config copied from Josean Martinez

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -9,8 +9,52 @@ return {
     build = "make tiktoken", -- Only on MacOS or Linux
     opts = {
       debug = true, -- Enable debugging
-      -- See Configuration section for rest
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+        debounce = 75,
+        keymap = {
+          accept = "<C-l>",
+          next = "<C-j>",
+          prev = "<C-k>",
+          dismiss = "<C-h>",
+        },
+      },
+      panel = {
+        enabled = true,
+        auto_refresh = true,
+        keymap = {
+          jump_prev = "[[",
+          jump_next = "]]",
+          accept = "<CR>",
+          refresh = "gr",
+          open = "<M-CR>",
+        },
+      },
+      filetypes = {
+        yaml = false,
+        markdown = true,
+        help = false,
+        gitcommit = true,
+        gitrebase = true,
+        hgcommit = true,
+        svn = true,
+        cvs = true,
+        ["."] = false,
+      },
+      copilot_node_command = 'node', -- Node.js version must be > 16.x
+      server_opts_overrides = {},
     },
     -- See Commands section for default commands if you want to lazy load on them
+    config = function()
+      local chat = require("CopilotChat")
+      local actions = require("CopilotChat.actions")
+      -- local pick = require("CopilotChat.pick")
+
+      vim.keymap.set({ 'n', 'v' }, '<leader>aa', chat.toggle, { desc = 'AI Toggle' })
+      vim.keymap.set({ 'n', 'v' }, '<leader>ax', chat.reset, { desc = 'AI Reset' })
+      -- vim.keymap.set({ 'n', 'v' }, '<leader>ah', pick(actions.help_actions), { desc = 'AI Help Actions' })
+      -- vim.keymap.set({ 'n', 'v' }, '<leader>ap', pick(actions.prompt_actions), { desc = 'AI Prompt Actions' })
+    end,
   },
 }
