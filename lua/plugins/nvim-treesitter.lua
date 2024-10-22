0b7517b596e32eafd1b3daedfc1e42d06b808ebc
Cristian Molinero <cristian.molinero@mercadolibre.com>
2024-10-22 18:37:06 -0300

    lint

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,39 +1,39 @@
 return { 
   'nvim-treesitter/nvim-treesitter',
   build = ':TSUpdate', 
-    config = function()
-      require'nvim-treesitter.configs'.setup {
-        -- A list of parser names, or "all" (the listed parsers MUST always be installed)
-        -- ensure_installed = { "lua", "vim", "vimdoc",  "markdown", "markdown_inline" , "json", "javascript", "tsx", "bash"},
+  config = function()
+    require'nvim-treesitter.configs'.setup {
+      -- A list of parser names, or "all" (the listed parsers MUST always be installed)
+      -- ensure_installed = { "lua", "vim", "vimdoc",  "markdown", "markdown_inline" , "json", "javascript", "tsx", "bash"},
 
-        -- Install parsers synchronously (only applied to `ensure_installed`)
-        sync_install = true,
+      -- Install parsers synchronously (only applied to `ensure_installed`)
+      sync_install = true,
 
-        -- Automatically install missing parsers when entering buffer
-        auto_install = true,
+      -- Automatically install missing parsers when entering buffer
+      auto_install = true,
 
-        -- List of parsers to ignore installing (or "all")
-        -- ignore_install = { "javascript" },
+      -- List of parsers to ignore installing (or "all")
+      -- ignore_install = { "javascript" },
 
-        highlight = {
-          enable = true,
+      highlight = {
+        enable = true,
 
-          -- NOTE: these are the names of the parsers and not the filetype.
-          disable = { "c", "rust" },
+        -- NOTE: these are the names of the parsers and not the filetype.
+        disable = { "c", "rust" },
 
-          -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
-          disable = function(lang, buf)
-            local max_filesize = 100 * 1024 -- 100 KB
-            local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-            if ok and stats and stats.size > max_filesize then
-              return true
-            end
-          end,
+        -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
+        disable = function(lang, buf)
+          local max_filesize = 100 * 1024 -- 100 KB
+          local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+          if ok and stats and stats.size > max_filesize then
+            return true
+          end
+        end,
 
-          -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-          additional_vim_regex_highlighting = false,
-        },
-        indent = { enable = true },
-      }
-    end,
-  }
+        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+        additional_vim_regex_highlighting = false,
+      },
+      indent = { enable = true },
+    }
+  end,
+}
