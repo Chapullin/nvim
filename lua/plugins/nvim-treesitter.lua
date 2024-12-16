e744ac6f9cdad34562432ef10525d7f7b2355319
Cristian Molinero <cristian.molinero@mercadolibre.com>
2024-12-16 12:44:22 -0300

    attempt to fix foldering folders

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,7 +1,12 @@
-return { 
+return {
   'nvim-treesitter/nvim-treesitter',
-  build = ':TSUpdate', 
+  build = ':TSUpdate',
   config = function()
+    -- Set folding options
+    local vim = vim
+    vim.opt.foldmethod = "expr"
+    vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+
     require'nvim-treesitter.configs'.setup {
       -- A list of parser names, or "all" (the listed parsers MUST always be installed)
       -- ensure_installed = { "lua", "vim", "vimdoc",  "markdown", "markdown_inline" , "json", "javascript", "tsx", "bash"},
@@ -19,7 +24,7 @@ return {
         enable = true,
 
         -- NOTE: these are the names of the parsers and not the filetype.
-        disable = { "c", "rust" },
+        -- disable = { "c", "rust" },
 
         -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
         disable = function(lang, buf)
@@ -41,5 +46,8 @@ return {
         foldexpr = 'nvim_treesitter#foldexpr()',
       },
     }
+    vim.cmd [[
+      autocmd BufReadPost * normal zR
+      ]]
   end,
 }
