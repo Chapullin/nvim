cf706419e86a5cadd11bbf99c9e1373d45be3c2a
Cristian Molinero <cristian.molinero@mercadolibre.com>
2024-12-16 12:45:16 -0300

    reformat file

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,20 +2,33 @@ return {
   {
     'nvim-telescope/telescope.nvim',
     tag = '0.1.8',
-    dependencies = { 'nvim-lua/plenary.nvim' }
-  },
-  {
-    'nvim-telescope/telescope-ui-select.nvim',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      {
+        'nvim-telescope/telescope-live-grep-args.nvim',
+        version="^1.0.0"
+      },
+      'nvim-telescope/telescope-ui-select.nvim',
+    },
     config = function()
-      require("telescope").setup {
+      local telescope=require('telescope')
+
+      -- Telescope config
+      telescope.setup({
+        defaults = {
+          layout_strategy = 'vertical',
+        },
         extensions = {
           ["ui-select"] = {
-            require("telescope.themes").get_dropdown {
-            }
+            require("telescope.themes").get_dropdown {}
           }
+
         }
-      }
-      require("telescope").load_extension("ui-select")
+      })
+
+      -- Load extensions
+      telescope.load_extension("live_grep_args")
+      telescope.load_extension("ui-select")
     end
-  }
+  },
 }
